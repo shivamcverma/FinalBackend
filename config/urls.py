29522e3629583811c58486f93c8a286061d55235
Achyutashivam <shivam@achyutagroup.com>
2026-05-18 12:05:24 +0530

    first commit

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,34 @@
 from django.urls import path, include
 from django.contrib import admin
+from drf_yasg.views import get_schema_view, schema_view
+from drf_yasg import openapi
+
+from properties import permissions
+
+
+# 🔥 SWAGGER CONFIG
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tolet API",
+        default_version='v1',
+        description="Tolet Backend APIs",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/properties/', include('properties.urls')),
+    path(
+    'swagger/',
+    schema_view.with_ui(
+        'swagger',
+        cache_timeout=0
+    )
+),
 ]
+
+
